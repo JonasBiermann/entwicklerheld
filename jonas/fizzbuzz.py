5d4d3394464dcd53551074bdf518d53729e84646
jonasmac <jojabiermann@gmail.com>
2022-04-03 13:59:42 +0200

    added fizzbuzz code

---
+++ jonas/fizzbuzz.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+def fizzbuzz(number: int):
+    if number % 3 == 0:
+        return('fizz')
+    if number % 5 == 0:
+        return("buzz")
+    if number % 3 == 0 and number % 5 == 0:
+        return("fizzbuzz")
+    else: return(number)
